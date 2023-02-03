f5ad8c9326d073a2f8c61abc32b38333c52112b2
Brad Harris <bmharris@gmail.com>
2023-02-03 00:35:56 +0000

    fixing styles

--- components/dashboard/src/teams/git-auth/GitIntegrationsList.tsx
+++ components/dashboard/src/teams/git-auth/GitIntegrationsList.tsx
@@ -24,11 +24,11 @@ export const GitIntegrationsList: FunctionComponent<Props> = ({ providers }) => 
         <>
             {providers.length === 0 ? (
                 <div className="w-full flex h-80 mt-2 rounded-xl bg-gray-100 dark:bg-gray-900">
-                    <div className="m-auto text-center">
+                    <div className="m-auto text-center px-8">
                         <h3 className="self-center text-gray-500 dark:text-gray-400 mb-4">No Git Integrations</h3>
-                        <div className="text-gray-500 mb-6">
-                            In addition to the default Git Providers you can authorize
-                            <br /> with a self-hosted instance of a provider.
+                        <div className="text-gray-500 mb-6 max-w-md">
+                            In addition to the default Git Providers you can authorize with a self-hosted instance of a
+                            provider.
                         </div>
                         <button className="self-center" onClick={onCreate}>
                             New Integration
@@ -37,10 +37,8 @@ export const GitIntegrationsList: FunctionComponent<Props> = ({ providers }) => 
                 </div>
             ) : (
                 <>
-                    <div className="mt-3 flex mt-0">
-                        <button onClick={onCreate} className="ml-2">
-                            New Integration
-                        </button>
+                    <div className="mt-3">
+                        <button onClick={onCreate}>New Integration</button>
                     </div>
 
                     <ItemsList className="pt-6">
